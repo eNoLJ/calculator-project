04a93c00c2a4bdbce0766ec5d112a3866fadd94f
eNoLJ <derosatam76@gmail.com>
2025-12-04 17:15:57 +0900

    feat: 반복문을 사용하여 무한으로 계산을 진행하도록 구현
    종료를 알려주는 exit 문자열을 입력하기 전까지 무한으로 계산을 진행하도록 반복문 구현

--- src/com/enolj/calculator/Main.java
+++ src/com/enolj/calculator/Main.java
@@ -7,53 +7,60 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int firstNum = 0;
-        int secondNum = 0;
-        try {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            firstNum = scanner.nextInt();
-            if (firstNum < 0) {
-                throw new InputMismatchException();
-            }
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            secondNum = scanner.nextInt();
-            if (secondNum < 0) {
-                throw new InputMismatchException();
-            }
-        } catch (InputMismatchException inputMismatchException) {
-            System.out.println("올바른 숫자를 입력해 주세요.");
-            scanner.nextLine();
-        }
-
-        String operator = "";
-        try {
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            operator = scanner.next();
-            if (!(operator.equals("+") || operator.equals("-") || operator.equals("x") || operator.equals("/"))) {
-                throw new InputMismatchException();
-            }
-        }  catch (InputMismatchException inputMismatchException) {
-            System.out.println("올바른 연산기호를 입력해 주세요.");
-        }
-
-        int result = 0;
-        try {
-            result = switch (operator) {
-                case "+" -> firstNum + secondNum;
-                case "-" -> firstNum - secondNum;
-                case "x" -> firstNum * secondNum;
-                case "/" -> {
-                    if (secondNum == 0) {
-                        throw new InputMismatchException();
-                    }
-                    yield firstNum / secondNum;
+        while (true) {
+            int firstNum = 0;
+            int secondNum = 0;
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                firstNum = scanner.nextInt();
+                if (firstNum < 0) {
+                    throw new InputMismatchException();
                 }
-                default -> result;
-            };
-        } catch (InputMismatchException inputMismatchException) {
-            System.out.println("분모를 0으로 나눌 수 없습니다.");
-        }
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                secondNum = scanner.nextInt();
+                if (secondNum < 0) {
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException inputMismatchException) {
+                System.out.println("올바른 숫자를 입력해 주세요.");
+                scanner.nextLine();
+            }
 
-        System.out.println("결과: " + result);
+            String operator = "";
+            try {
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                operator = scanner.next();
+                if (!(operator.equals("+") || operator.equals("-") || operator.equals("x") || operator.equals("/"))) {
+                    throw new InputMismatchException();
+                }
+            }  catch (InputMismatchException inputMismatchException) {
+                System.out.println("올바른 연산기호를 입력해 주세요.");
+            }
+
+            int result = 0;
+            try {
+                result = switch (operator) {
+                    case "+" -> firstNum + secondNum;
+                    case "-" -> firstNum - secondNum;
+                    case "x" -> firstNum * secondNum;
+                    case "/" -> {
+                        if (secondNum == 0) {
+                            throw new InputMismatchException();
+                        }
+                        yield firstNum / secondNum;
+                    }
+                    default -> result;
+                };
+            } catch (InputMismatchException inputMismatchException) {
+                System.out.println("분모를 0으로 나눌 수 없습니다.");
+            }
+
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String answer = scanner.next();
+            if (answer.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
     }
 }
