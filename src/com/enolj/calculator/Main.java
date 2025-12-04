21b3edcb6c3e1ae2d75b1542f41af75a76163e7e
eNoLJ <derosatam76@gmail.com>
2025-12-04 16:29:32 +0900

    feat: 양의 정수(0 포함)를 입력받기 구현
    양의 정수 두개를 입력 받도록 구현, 음수이거나 숫자가 아닐 경우 InputMismatchException 발생

--- src/com/enolj/calculator/Main.java
+++ src/com/enolj/calculator/Main.java
@@ -1,7 +1,26 @@
 package com.enolj.calculator;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int firstNum = scanner.nextInt();
+            if (firstNum < 0) {
+                throw new InputMismatchException();
+            }
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int secondNum = scanner.nextInt();
+            if (secondNum < 0) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("올바른 숫자를 입력해 주세요.");
+            scanner.nextLine();
+        }
     }
 }
