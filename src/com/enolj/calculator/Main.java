392503a8345ad55629b38758af99223fcadc0bea
eNoLJ <derosatam76@gmail.com>
2025-12-04 16:48:13 +0900

    feat: 사직연산 기호를 입력받기 구현
    사칙연산 기호를 입력받는 operator 변수 구현, 올바른 연산기호가 아닐 경우 InputMismatchException 발생

--- src/com/enolj/calculator/Main.java
+++ src/com/enolj/calculator/Main.java
@@ -7,20 +7,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int firstNum;
+        int secondNum;
         try {
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum = scanner.nextInt();
+            firstNum = scanner.nextInt();
             if (firstNum < 0) {
                 throw new InputMismatchException();
             }
             System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum = scanner.nextInt();
+            secondNum = scanner.nextInt();
             if (secondNum < 0) {
                 throw new InputMismatchException();
             }
         } catch (InputMismatchException inputMismatchException) {
             System.out.println("올바른 숫자를 입력해 주세요.");
             scanner.nextLine();
+        }
+
+        String operator;
+        try {
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            operator = scanner.next();
+            if (!(operator.equals("+") || operator.equals("-") || operator.equals("x") || operator.equals("/"))) {
+                throw new InputMismatchException();
+            }
+        }  catch (InputMismatchException inputMismatchException) {
+            System.out.println("올바른 연산기호를 입력해 주세요.");
         }
     }
 }
