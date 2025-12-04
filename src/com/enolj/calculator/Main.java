9bdf4f11775bda83736fde62bc148ce6fcbc1acd
eNoLJ <derosatam76@gmail.com>
2025-12-04 16:59:45 +0900

    feat: 연산을 진행한 후 결과값 출력하기 구현
    양의 정수 2개와 사칙연산 기호를 바탕으로 연산 후 결과를 출력하는 기능 구현, 분모를 0으로 나눌 시 InputMismatchException 발생

--- src/com/enolj/calculator/Main.java
+++ src/com/enolj/calculator/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int firstNum;
-        int secondNum;
+        int firstNum = 0;
+        int secondNum = 0;
         try {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             firstNum = scanner.nextInt();
@@ -25,7 +25,7 @@ public class Main {
             scanner.nextLine();
         }
 
-        String operator;
+        String operator = "";
         try {
             System.out.print("사칙연산 기호를 입력하세요: ");
             operator = scanner.next();
@@ -35,5 +35,25 @@ public class Main {
         }  catch (InputMismatchException inputMismatchException) {
             System.out.println("올바른 연산기호를 입력해 주세요.");
         }
+
+        int result = 0;
+        try {
+            result = switch (operator) {
+                case "+" -> firstNum + secondNum;
+                case "-" -> firstNum - secondNum;
+                case "x" -> firstNum * secondNum;
+                case "/" -> {
+                    if (secondNum == 0) {
+                        throw new InputMismatchException();
+                    }
+                    yield firstNum / secondNum;
+                }
+                default -> result;
+            };
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("분모를 0으로 나눌 수 없습니다.");
+        }
+
+        System.out.println("결과: " + result);
     }
 }
